1c2d2240278eaf009339fff6360ae4a8fdae575b
hmous <hmoust123@gmail.com>
2022-04-11 20:26:54 -0400

    ajoute de int a et afficher la valeur de a

--- src/conversion.java
+++ src/conversion.java
@@ -2,5 +2,9 @@ public class conversion {
     public static void main(String[] args) {
         System.out.println("bonjour git");
 
+        int a = 25;
+
+        System.out.println("la valeur de a = " + a);
+
     }
 }
